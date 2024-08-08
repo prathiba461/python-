b920944cb7624b6bb56dc42dde9dd4da46ca1b00
prathiba461 <prathiba1503@gmail.com>
2024-08-08 06:55:08 +0100

    Add files via upload

---
+++ exp 4.py
@@ -0,0 +1,7 @@
+a=25
+b=25
+sum=a+b
+sub=a-b
+print("the value of a {} and b is {}".format(a,b))
+print("{2} is the sum of {0} and {1}".format(a,b,sum))
+print("{2} is the sub of {0} and {1}".format(a,b,sub))
